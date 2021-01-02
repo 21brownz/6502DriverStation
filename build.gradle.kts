5789ef168d2e71308d9d6a487c77ae258cefa261
Zach Brown <21brownz@da.org>
2021-01-01 22:02:28 -0700

    added stuff

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "FRC6502"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven { setUrl("https://jitpack.io") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("uk.co.electronstudio.sdl2gdx:sdl2gdx:1.0.+")
+    implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
+    implementation("com.github.WilliamAHartman:Jamepad:1.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+
+import com.studiohartman.jamepad.*
+
+//main module for the entire project. Put the stuff you want to run in fun main()
+
+fun main(){
+    val controllers = ControllerManager()
+    controllers.initSDLGamepad()
+    while (true) {
+        val currState: ControllerState = controllers.getState(0)
+        when {
+            currState.a -> {
+                println("\"A\" on \"" + currState.controllerType + "\" is pressed")
+            }
+            currState.b -> {
+                println("\"B\" on \"" + currState.controllerType + "\" is pressed")
+            }
+            currState.x -> {
+                println("\"X\" on \"" + currState.controllerType + "\" is pressed")
+            }
+            currState.y -> {
+                println("\"Y\" on \"" + currState.controllerType + "\" is pressed")
+            }
+        }
+    }
+}
